5ae93c59cc959c66da755d71896ec2d01ac7aa83
Rodrigo <quisperodrigo00@gmail.com>
2026-07-09 02:04:33 -0500

    hola

--- front-VistaInopinada/app/page.tsx
+++ front-VistaInopinada/app/page.tsx
@@ -1,5 +1,24 @@
+<<<<<<< Updated upstream
 import { redirect } from "next/navigation"
 
 export default function HomePage() {
   redirect("/login")
+=======
+"use client"
+
+import { useEffect } from "react"
+import { useRouter } from "next/navigation"
+
+export default function HomePage() {
+  const router = useRouter()
+
+  useEffect(() => {
+    // Redirige al login si no estamos en modo export
+    if (typeof window !== 'undefined' && !window.location.pathname.includes('index.html')) {
+      router.replace("/login")
+    }
+  }, [router])
+
+  return <div /> // Renderiza un div vacío mientras redirige
+>>>>>>> Stashed changes
 }
